28dce7dc15455056592c334963345cbf84cdc79f
Justin Bodeutsch <lefthand@gmail.com>
2011-09-18 23:29:20 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,177 @@
+var express = require('express');
+
+var ArticleProvider = require('./articleProvider.js').ArticleProvider;
+var connect = require('express/node_modules/connect');
+var RedisStore = require('connect-redis')(express);
+var sessionStore = new RedisStore();
+var redis = require("redis");
+var client = redis.createClient();
+
+var Session = connect.middleware.session.Session,
+    parseCookie = connect.utils.parseCookie
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+var app = module.exports = express.createServer();
+
+// = Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    store: sessionStore,
+    secret: 'shhhhhh',
+    key: 'my.sid',
+    cookie: {maxAge: 31557600000 }
+  }));
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+var userProvider = new ArticleProvider('users');
+var countProvider = new ArticleProvider('count');
+
+countProvider.getUniqueId('saves', function(error, count) { 
+  if (error) {
+    console.log('Could not determine count');
+  }
+  console.log('The count is: ' + count);
+});
+
+client.incr("connections", function (err, reply) {
+  console.log("This has been run " + reply + " times!");
+});
+
+// Routes
+function loadUser(req, res, next) {
+  if (req.session.user) {
+    console.log('Logged In: ' + req.session.user.username);
+    req.user = req.session.user;
+  }
+  else {
+    req.user = {};
+  }
+  next();
+}
+
+app.get('/', loadUser, function(req, res){
+  res.render('index', {
+    title: 'Fun', loggedInUser:req.user 
+  });
+});
+
+app.get('/about', loadUser, function(req, res){
+  res.render('about', {
+    title: 'About', loggedInUser:req.user 
+  });
+});
+
+app.get('/posts', loadUser, function(req, res){
+  userProvider.findAll(function(error, posts) { 
+    res.render('posts', { posts: posts, title: 'Posts', loggedInUser:req.user  });
+  });
+});
+
+app.get('/users', loadUser, function(req, res){
+  userProvider.findAll(function(error, users) { 
+    res.render('users', { users: users, title: 'Users', loggedInUser:req.user });
+  });
+});
+
+app.get('/user/add', loadUser, function(req, res, next){
+  res.render('users/add', { title: 'New User', loggedInUser:req.user });
+});
+
+app.post('/user/add', loadUser, function(req, res, next){
+  countProvider.getUniqueId('users', function(error, id) {
+    userProvider.save({
+      _id: id,
+      name: req.param('name'),
+      email: req.param('email')
+    }, function( error, docs) {
+      res.redirect('/users')
+    });
+  });
+});
+
+app.get('/user/:id/edit', loadUser, function(req, res, next){
+  userProvider.findById(req.params.id, function(error, user) {
+    res.render('users/edit', { user: user, title: 'User ' + req.params.id, loggedInUser:req.user });
+  });
+});
+
+app.get('/user/:id/remove', loadUser, function(req, res, next){
+  if (req.params.id === 'null') {
+    req.params.id = '';
+  }
+  userProvider.remove(req.params.id, function(error, user) {
+    res.redirect('/users')
+  });
+});
+
+app.post('/user/:id/submit', loadUser, function(req, res){
+  userProvider.update({
+    _id: req.params.id,
+    data: {
+      name: req.param('name'),
+      username: req.param('username'),
+      email: req.param('email'),
+      password: req.param('password'),
+      is_root: req.param('is_root'),
+      is_admin: req.param('is_admin')
+    }
+  }, function( error, docs) {
+    res.redirect('/user/' + req.params.id);
+  });
+});
+
+app.get('/user/:id', loadUser, function(req, res, next){
+  userProvider.findById(req.params.id, function(error, user) {
+    res.render('users/show', { user: user, title: 'User ' + req.params.id, loggedInUser:req.user });
+  });
+});
+
+app.post('/login', loadUser, function(req, res){
+  userProvider.findOne({username: req.param('username')}, function (error, user) {
+    if (error || !user) {
+      console.log('Couldn\'t find user! ' + req.param('username'));
+    }
+    else {
+      if (req.param('password') &&  req.param('password') === user.password) {
+        if (req.session) {
+          console.log('Someone logged in! ' + req.param('username') + ' ' + user._id);
+          req.session.user = user;
+        }
+      }
+      else {
+        console.log('Wrong password for ' + req.param('username') + '!');
+      }
+    }
+    res.redirect('back');
+  });
+});
+
+app.get('/logout', function(req, res){
+    if (req.session.user) {
+      console.log('Logging Out: ' + req.session.user.username);
+      delete req.session.user;
+    }
+    res.redirect('/');
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ articleProvider.js
@@ -0,0 +1,132 @@
+var BSON = require("mongodb").BSON;
+var ObjectID = require("mongodb").ObjectID;
+var MongoProvider = require('./mongoProvider.js').MongoProvider;
+
+var mongoProvider = new MongoProvider('localhost', 27017);
+
+ArticleProvider = function(collection) {
+  this.collection = collection;
+};
+
+ArticleProvider.prototype.getCollection = function(callback) {
+  mongoProvider.db.collection(this.collection, function(error, user_collection) {
+    if( error ) callback(error);
+    else callback(null, user_collection);
+  });
+}
+
+ArticleProvider.prototype.findAll = function(callback) {
+  this.getCollection(function(error, user_collection) {
+    if( error ) callback(error)
+    else {
+      user_collection.find().toArray(function(error, results) {
+        if( error ) callback(error)
+        else callback( null, results)
+      });
+    }
+  });
+};
+
+ArticleProvider.prototype.findById = function(id, callback) {
+  this.getCollection(function(error, user_collection) {
+    if( error ) callback(error)
+    else {
+      user_collection.findOne({_id: parseInt(id)}, function(error, result) {
+        if( error ) callback(error)
+        else callback( null, result)
+      });
+    }
+  });
+}
+
+ArticleProvider.prototype.findBy_Id = function(id, callback) {
+  this.getCollection(function(error, user_collection) {
+    if( error ) callback(error)
+    else {
+      user_collection.findOne({_id: user_collection.db.bson_serializer.ObjectID.createFromHexString(id)}, function(error, result) {
+        if( error ) callback(error)
+        else callback( null, result)
+      });
+    }
+  });
+}
+
+ArticleProvider.prototype.findOne = function(find, callback) {
+  this.getCollection(function(error, game_collection) {
+    if( error ) callback(error)
+    else {
+      game_collection.findOne(find, function(error, result) {
+        if( error ) callback(error)
+        else callback( null, result)
+      });
+    }
+  });
+}
+
+ArticleProvider.prototype.getUniqueId = function(id, callback) {
+  this.getCollection(function(error, user_collection) {
+    if( error ) callback(error)
+    else {
+      user_collection.findAndModify({_id: id}, [['_id','asc']], {$inc: {count:1}}, {upsert:true,new:true}, function(error, result) {
+        if( error ) callback(error)
+        else {
+          callback( null, result.count)
+        }
+      });
+    }
+  });
+}
+
+ArticleProvider.prototype.save = function(users, callback) {
+  this.getCollection(function(error, user_collection) {
+    if( error ) callback(error)
+    else {
+      if (typeof(users.length) == "undefined") {
+        users = [users];
+      }
+      for (var i = 0; i < users.length; i++ ) {
+        user = users[i];
+        user.created_at = new Date();
+        user.modified_at = new Date();
+        if (user._id === 'undefined' && user.id) {
+          user._id = user.id;
+        }
+        user_collection.save(user, function() {
+          callback(null, user);
+        });
+      }
+
+    }
+  });
+};
+
+ArticleProvider.prototype.update = function(user, callback) {
+  this.getCollection(function(error, user_collection) {
+    if( error ) callback(error)
+    else {
+      user.data.modified_at = new Date();
+//      user_collection.update({_id: user_collection.db.bson_serializer.ObjectID.createFromHexString(user._id)}, {$set: user.data}, {multi:true}, function(err) {
+      user_collection.update({_id: parseInt(user._id)}, {$set: user.data}, {multi:true,safe:true}, function(err) {
+          if (err) console.warn(err.message);
+          else console.log('successfully updated: ' + user._id);
+          callback(null, user);
+      });
+    }
+  });
+};
+
+ArticleProvider.prototype.remove = function(id, callback) {
+  this.getCollection(function(error, user_collection) {
+    if( error ) callback(error)
+    else {
+//      user_collection.remove({_id: user_collection.db.bson_serializer.ObjectID.createFromHexString(id)} , function(err) {
+      user_collection.remove({_id: parseInt(id)} , function(err) {
+          if (err) console.warn(err.message);
+          else console.log('successfully removed: ' + id);
+          callback(null, id);
+      });
+    }
+  });
+};
+
+exports.ArticleProvider = ArticleProvider;
