0cc3322fac4a5249815596456548da0918f945a3
Justin Bodeutsch <lefthand@gmail.com>
2012-01-15 01:26:40 -0800

    Added a forgot password link which will reset the password to a random string. Next step will be to email the user a confirmation they need to click to get it reset.

--- app.js
+++ app.js
@@ -97,6 +97,19 @@ Array.prototype.unique = function() {
   return r;
 };
 
+String.prototype.randomString = function(stringLength) {
+  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  if (!stringLength>0) {
+    var stringLength = 8;
+  }
+  var randomString = '';
+  for (var i=0; i<stringLength; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    randomString += chars.substring(rnum,rnum+1);
+  }
+  return randomString; 
+}
+
 // connect to the db and make the collections available globally
 var db = mongo.db('localhost:27017/bootstrap')
 postDb = db.collection('post');
