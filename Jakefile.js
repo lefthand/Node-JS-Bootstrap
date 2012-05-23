18f63d96558ea4573cf636fb956949645aa06bcc
McLeopold <mcleopold@gmail.com>
2012-05-22 23:34:36 -0700

    Update jake
    Correctly copy all files needed for proper deployment.

--- Jakefile.js
+++ Jakefile.js
@@ -1,4 +1,6 @@
-var exec = require('tooljs').exec;
+var exec = require('tooljs').exec
+  , path = require('path')
+;
 
 task('default', ['deploy']);
 
@@ -9,7 +11,12 @@ task('clean', [], function () {
 }, true);
 
 task('deploy', ['clean'], function () {
+  var dest = 'deploy';
   exec([], {verbose: true}, function () { complete(); })
-    .cp('public', 'deploy', {recursive: true})
+    .cp('lib', dest, {recursive: true})
+    .cp('views', dest, {recursive: true})
+    .cp('public', dest, {recursive: true})
+    .cp('app.js', dest)
+    .cp('configDefault.js', path.join(dest, 'configLocal.js'))
   ;
 });
